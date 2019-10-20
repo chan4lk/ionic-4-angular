866aa03e9737d3f2838dac38294761ad5127a060
Chandima Ranaweera <chan4lk@gmail.com>
2019-10-20 11:19:33 +0530

    video loaded

--- src/app/acivity-video/acivity-video.page.ts
+++ src/app/acivity-video/acivity-video.page.ts
@@ -4,22 +4,18 @@ import { ModalController, NavParams } from '@ionic/angular';
 @Component({
   selector: 'app-acivity-video',
   templateUrl: './acivity-video.page.html',
-  styleUrls: ['./acivity-video.page.scss'],
+  styleUrls: ['./acivity-video.page.scss']
 })
 export class AcivityVideoPage implements OnInit {
   videoURL: string;
-  
-  constructor(
-    navParams: NavParams,
-    private modalController: ModalController) { 
-      this.videoURL = navParams.get('videoURL');
-    }
 
-  ngOnInit() {
+  constructor(navParams: NavParams, private modalController: ModalController) {
+    this.videoURL = navParams.get('videoURL');
   }
+
+  ngOnInit() {}
 
   closeModal() {
     this.modalController.dismiss();
   }
-
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Activity } from '../models/Activity';
 import { ActivityService } from '../activity.service';
@@ -8,14 +8,17 @@ import { ActivityService } from '../activity.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   activities$: Observable<Activity[]>;
 
-  constructor(activityService: ActivityService) {
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit() {
     setTimeout(() => {
-      this.activities$ = activityService.getAllActivities();
+      this.activities$ = this.activityService.getAllActivities();
     }, 3000);
   }
+
 
 }
